24f9075eb660b9ae81fa62ae0cc6bafca93e17b9
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-05-04 23:21:10 +0100

    extract logic to parse logic from csv file into its own class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader'
 
-const matches = fs.readFileSync('football.csv', {
-  // leave this flag below to recieve a string of the CSV file
-  encoding: 'utf-8'
-})
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
-//  enum (enumberation) is an object that stores some very closely related values
+const reader = new CsvFileReader('football.csv')
+reader.read()
+//  enum (enumeration) is an object that stores some very closely related values
 enum MatchResult {
+  // when declairing an enum it also creates a type called MatchResult
   HomeWin = 'H',
   AwayWin = 'A',
   Draw = 'D'
@@ -17,7 +12,7 @@ enum MatchResult {
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of reader.data) {
   // the 'H' is a magic string, we will get rid of this with typescript
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++
