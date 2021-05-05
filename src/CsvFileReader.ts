effdce19e492d354cfadb52909e3627b51adc6b6
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-05-05 09:31:22 +0100

    use generics to assign types in Csvfile

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -2,13 +2,13 @@ import fs from 'fs'
 import { MatchResult } from './MatchResult'
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
-export abstract class CsvFileReader {
+export abstract class CsvFileReader<T> {
 
-  data: MatchData[] = [];
+  data: T[] = [];
 
   constructor(public filename: string) { }
 
-  abstract mapRow(row: string[]): MatchData
+  abstract mapRow(row: string[]): T
 
   read(): void {
     this.data = fs.readFileSync(this.filename, {
